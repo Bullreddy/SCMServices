21b550a0df0aa9a5cc77a5b23b3c293676b0fdd1
Lokesh <lokesh.devareddy@gmail.com>
2018-07-08 22:14:50 +0530

    Allow access for all origin

--- src/main/java/com/scm/services/config/JwtAuthenticationFilter.java
+++ src/main/java/com/scm/services/config/JwtAuthenticationFilter.java
@@ -40,7 +40,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String header = req.getHeader(Constants.HEADER_STRING);
         HttpServletResponse response = (HttpServletResponse) res;
     	HttpServletRequest request = (HttpServletRequest) req;
-    	response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+    	response.setHeader("Access-Control-Allow-Origin", "*");
     	response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
     	response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
